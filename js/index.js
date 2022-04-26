7c624144aa43afc89481e29551226f108358ae7d
Allawi465 <allawi465@gmail.com>
2022-04-26 05:35:48 +0200

    .

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const navOpenClos = () => {
+    const hamburger = document.querySelector(".hamburger");
+    const nav = document.querySelector("nav");
+
+    hamburger.addEventListener("click", () => {
+        nav.classList.toggle("aktiv");
+        hamburger.classList.toggle("burger");
+
+    })
+};
+
+navOpenClos();
