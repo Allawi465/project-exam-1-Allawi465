8821188ea3b4eaff5064e4fa57a11d0422ab3b61
Allawi465 <allawi465@gmail.com>
2022-04-30 20:42:11 +0200

    .

--- js/index.js
+++ js/index.js
@@ -11,17 +11,69 @@ const navOpenClos = () => {
 
 navOpenClos();
 
-/* const prevBtn = document.querySelector(".pre-btn");
+
+
+const slidesCard   = document.querySelectorAll(".card")
+const prevBtn = document.querySelector(".pre-btn");
 const nextBtn = document.querySelector(".next-btn");
+let counter = 0;
 
-function next() {
-    console.log("heiii")
-};
 
-nextBtn.onclick = next;
+/* slidesCard.forEach(function(card, index){
+    if (window.innerWidth > 1050) {
+        card.style.left = `${index * 25}%`;
+        console.log("25")
+    } 
+    if (window.innerWidth < 1050) {
+        card.style.left = `${index * 97.8}%`;
+        console.log("100")
+    } 
+})
 
-function prev() {
-    console.log("heiii")
-};
+slidesCard.forEach(function(card, index){
+    window.addEventListener("reset", function() {
+        if (window.innerWidth > 1050) {
+            card.style.left = `${index * 25}%`;
+            console.log("25")
+        } 
+        if (window.innerWidth < 1050) {
+            card.style.left = `${index * 97.8}%`;
+            console.log("100")
+        } 
+    })
+}) */
 
-prevBtn.onclick = prev; */
+
+
+
+slidesCard.forEach(function(card, index){
+    card.style.left = `${index * 97.8}%`;
+}); 
+
+nextBtn.addEventListener("click", function () {
+    counter++;
+    slides()
+});
+
+prevBtn.addEventListener("click", function () {
+    counter--;
+    slides()
+});
+
+function slides() {
+    if (counter < slidesCard.length - 1 ) {
+        nextBtn.style.display = "block";
+    } else {
+        nextBtn.style.display = "none";
+    }
+    if (counter > 0 ) {
+        prevBtn.style.display = "block";
+    } else {
+        prevBtn.style.display = "none";
+    } 
+    slidesCard.forEach(function(card){
+        card.style.transform = `translateX(-${counter * 100}%)`;
+    });
+}
+
+prevBtn.style.display = "none"
