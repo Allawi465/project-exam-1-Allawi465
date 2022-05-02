4fe3a105991f9070db835676323bd79e12432fe3
Allawi465 <allawi465@gmail.com>
2022-05-02 15:21:39 +0200

    .

--- js/index.js
+++ js/index.js
@@ -19,6 +19,8 @@ const nextBtn = document.querySelector(".next-btn");
 let widthCard = slider.offsetWidth;
 let counter = 0;
 
+console.log(widthCard)
+
 window.addEventListener("resize", function() {
     widthCard = slider.offsetWidth
 });
@@ -28,11 +30,10 @@ nextBtn.addEventListener("click", function () {
     prevBtn.style.display = "block" 
 
     movingCard.style.transform = "translateX(" + counter * -widthCard + "px)";
-
-    if (movingCard.offsetWidth - counter * widthCard < counter * widthCard) {
-        nextBtn.style.display = "none";
-    }
     
+    if  (movingCard.offsetWidth - (counter * widthCard) <  widthCard) {
+        nextBtn.style.display = "none";
+    } 
 });
 
 prevBtn.addEventListener("click", function () {
