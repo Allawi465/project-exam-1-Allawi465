e4cf56675b563d9913b2775b9d299e219e64b96b
Allawi465 <allawi465@gmail.com>
2022-05-24 20:13:28 +0200

    .

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
-const navOpenClos = () => {
-    const hamburger = document.querySelector(".hamburger");
-    const nav = document.querySelector("nav");
-    const header = document.querySelector("header")
+const header = document.querySelector("header");
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
 
+const navOpenClos = () => {
     hamburger.addEventListener("click", () => {
         nav.classList.toggle("aktiv");
         hamburger.classList.toggle("burger");
-        header.classList.toggle("headerHeight");
+        header.classList.toggle("headerHeight");    
     })
 };
 
-navOpenClos();
+navOpenClos()
+
+window.addEventListener("resize", function() {
+    if (window.innerWidth >= 1082) {
+        header.classList.remove("headerHeight");
+        nav.classList.remove("aktiv");
+        hamburger.classList.remove("burger");
+    } else {
+        header.classList.add("header");
+        nav.classList.add("nav");
+        hamburger.classList.add("hamburger");
+    }
+  });
