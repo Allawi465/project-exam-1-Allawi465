54e8fc2d074526db10c9d854d161016dc21e7dfa
Allawi465 <allawi465@gmail.com>
2022-05-25 19:03:33 +0200

    .

--- js/index.js
+++ js/index.js
@@ -8,6 +8,13 @@
         hamburger.classList.toggle("burger");
         header.classList.toggle("headerHeight");    
     })
+    window.addEventListener("resize", function() {
+        if (window.innerWidth > 1082) {
+            header.classList.remove("headerHeight");
+            nav.classList.remove("aktiv");
+            hamburger.classList.remove("burger");
+        } 
+    }); 
 };
 
 
